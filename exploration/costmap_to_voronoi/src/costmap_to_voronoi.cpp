bfdee805aa78665f057e17a90a948236dda78cf3
shuinoue <shuinoue0817@gmail.com>
2019-02-22 17:41:43 +0900

    add same function for robot2

--- exploration/costmap_to_voronoi/src/costmap_to_voronoi.cpp
+++ exploration/costmap_to_voronoi/src/costmap_to_voronoi.cpp
@@ -13,22 +13,22 @@ geometry_msgs::PoseStamped start_pose_set(std::string& frame_id)
   start_.pose.orientation.w=1.0;
   return start_;
 }
-void robot1_final_target_CB(const geometry_msgs::PoseStamped &goal)
+void robot_final_target_CB(const geometry_msgs::PoseStamped &goal)
 {
-  std::cout << "robot1_final_target_CB" << std::endl;
+  std::cout << "robot_final_target_CB" << std::endl;
   std::string name;
-  std::string robot1_frame_id_ = "/robot1/map";
+  std::string robot_frame_id_;
   ros::NodeHandle p("~");
   p.getParam("topic_ns", name);
-  CostmapToVoronoi r1_CB_ctv(name);
+  p.getParam(name+"global_frame",robot_frame_id_);
+  CostmapToVoronoi ctv(name);
   geometry_msgs::PoseStamped start_,goal_;
   std::vector<geometry_msgs::PoseStamped> plan;
-  start_ = start_pose_set(robot1_frame_id_);
+  start_ = start_pose_set(robot_frame_id_);
   goal_ = goal;
-  std::cout << "voronoiProcessが怪しい..." << std::endl;
-  r1_CB_ctv.voronoiProcess(start_,goal_,plan);
+  ctv.voronoiProcess(start_,goal_,plan);
 }
-
+/*
 void robot2_final_target_CB(const geometry_msgs::PoseStamped &goal)
 {
   std::cout << "robot1_final_target_CB" << std::endl;
@@ -44,34 +44,25 @@ void robot2_final_target_CB(const geometry_msgs::PoseStamped &goal)
   std::cout << "voronoiProcessが怪しい..." << std::endl;
   r2_CB_ctv.voronoiProcess(start_,goal_,plan);
 }
+*/
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "costmap_to_voronoi");
 
-  std::string name;
-  ros::NodeHandle p("~");
-  p.getParam("topic_ns", name);
-
-  CostmapToVoronoi ctv(name);
-
   geometry_msgs::PoseStamped start;
   geometry_msgs::PoseStamped goal;
   std::vector<geometry_msgs::PoseStamped> plan;
 
-  ros::NodeHandle nh1,nh2;
-  ros::Subscriber sub1,sub2;
-  ros::CallbackQueue queue1,queue2;
-  nh1.setCallbackQueue(&queue1);
-  nh2.setCallbackQueue(&queue2);
-  sub1 = nh1.subscribe("/robot1/final_target",1,&robot1_final_target_CB);
-  sub2 = nh2.subscribe("/robot2/final_target",1,&robot2_final_target_CB);
+  ros::NodeHandle nh;
+  ros::Subscriber sub;
+  ros::CallbackQueue queue;
+  nh.setCallbackQueue(&queue);
+  sub = nh.subscribe("final_target",1,&robot_final_target_CB);
 
   while(ros::ok()){
-    std::cout << "queue1 callone" << std::endl;
-    queue1.callOne(ros::WallDuration(1.0));
-    std::cout << "queue2 callone" << std::endl;
-    queue2.callOne(ros::WallDuration(1.0));
+    std::cout << "queue callone" << std::endl;
+    queue.callOne(ros::WallDuration(1.0));
   }
 
   return 0;
