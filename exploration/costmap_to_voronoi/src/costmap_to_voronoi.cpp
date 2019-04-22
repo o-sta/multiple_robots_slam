37c18f71ff18344469dd8fde81f54115cf4f6e02
shuinoue <shuinoue0817@gmail.com>
2019-04-22 20:01:08 +0900

    editing costmap to voronoi

--- exploration/costmap_to_voronoi/src/costmap_to_voronoi.cpp
+++ exploration/costmap_to_voronoi/src/costmap_to_voronoi.cpp
@@ -1,18 +1,54 @@
 #include "../include/costmap_to_voronoi.h"
-geometry_msgs::PoseStamped start_pose_set(std::string& frame_id)
+#include<nav_msgs/Odometry.h>
+
+ros::Subscriber odom_sub;
+ros::CallbackQueue odom_queue;
+geometry_msgs::PoseStamped start_g;
+
+void start_pose_set(const nav_msgs::Odometry::ConstPtr& robot_odom_);
+void robot_final_target_CB(const geometry_msgs::PoseStamped &goal);
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "costmap_to_voronoi");
+  ros::NodeHandle odom_nh;
+
+  ros::NodeHandle p("~");
+  geometry_msgs::PoseStamped start;
+  geometry_msgs::PoseStamped goal;
+  std::vector<geometry_msgs::PoseStamped> plan;
+  std::string robot_namespace_;
+  p.getParam("namespace",robot_namespace_);
+  std::cout << "namespace:" << robot_namespace_ << std::endl;
+
+  ros::NodeHandle nh;
+  ros::Subscriber sub;
+  ros::CallbackQueue queue;
+  nh.setCallbackQueue(&queue);
+  odom_nh.setCallbackQueue(&odom_queue);
+  sub = nh.subscribe("final_target",1,&robot_final_target_CB);
+  odom_sub = odom_nh.subscribe(robot_namespace_ + "/odom",1 ,start_pose_set);
+  while(ros::ok()){
+    std::cout << "queue callone" << std::endl;
+    queue.callOne(ros::WallDuration(1.0));
+  }
+
+  return 0;
+}
+
+
+void start_pose_set(const nav_msgs::Odometry::ConstPtr& robot_odom_)
 {
   std::cout << "start_pose_set" << std::endl;
-  geometry_msgs::PoseStamped start_;
-  start_.header.frame_id = frame_id;
-  std::cout << "start pose frame id : " << frame_id << std::endl;
-  start_.pose.position.x = 0.0;
-  start_.pose.position.y = 0.0;
-  start_.pose.position.z = 0.0;
-  start_.pose.orientation.x=0.0;
-  start_.pose.orientation.y=0.0;
-  start_.pose.orientation.z=0.0;
-  start_.pose.orientation.w=1.0;
-  return start_;
+  start_g.header.frame_id = robot_odom_->header.frame_id;
+  start_g.pose.position.x = robot_odom_->pose.pose.position.x;
+  start_g.pose.position.y = robot_odom_->pose.pose.position.y;
+  start_g.pose.position.z = robot_odom_->pose.pose.position.z;
+  start_g.pose.orientation.x=robot_odom_->pose.pose.orientation.x;
+  start_g.pose.orientation.y=robot_odom_->pose.pose.orientation.y;
+  start_g.pose.orientation.z=robot_odom_->pose.pose.orientation.z;
+  start_g.pose.orientation.w=robot_odom_->pose.pose.orientation.w;
+  std::cout << "end start_pose_set" << std::endl;
 }
 void robot_final_target_CB(const geometry_msgs::PoseStamped &goal)
 {
@@ -25,31 +61,10 @@ void robot_final_target_CB(const geometry_msgs::PoseStamped &goal)
   p.getParam(name+"/global_frame",robot_frame_id_);
   std::cout << "global frame : " << robot_frame_id_ << std::endl;
   CostmapToVoronoi ctv(name);
-  geometry_msgs::PoseStamped start_,goal_;
+  geometry_msgs::PoseStamped goal_;
   std::vector<geometry_msgs::PoseStamped> plan;
-  start_ = start_pose_set(robot_frame_id_);
+  odom_queue.callOne(ros::WallDuration(0.1));
   goal_ = goal;
-  ctv.voronoiProcess(start_,goal_,plan);
+  ctv.voronoiProcess(start_g,goal_,plan);
 }
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "costmap_to_voronoi");
-
-  geometry_msgs::PoseStamped start;
-  geometry_msgs::PoseStamped goal;
-  std::vector<geometry_msgs::PoseStamped> plan;
-
-  ros::NodeHandle nh;
-  ros::Subscriber sub;
-  ros::CallbackQueue queue;
-  nh.setCallbackQueue(&queue);
-  sub = nh.subscribe("final_target",1,&robot_final_target_CB);
-
-  while(ros::ok()){
-    std::cout << "queue callone" << std::endl;
-    queue.callOne(ros::WallDuration(1.0));
-  }
-
-  return 0;
-}
